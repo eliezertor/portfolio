0679f2cc164fef4764146faef1c87fd42f97f153
Eliezer Toribio <eliezertor@gmail.com>
2021-02-15 16:14:05 -0500

    added modal for nav menu

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,22 +1,13 @@
 import React, { useState } from "react";
 import Et from "../../assets/Logo/ET-white.png";
 import "./Navbar.scss";
+import Close from "../../assets/icons/window-close.svg";
 
 const Navbar = () => {
   let [ham, setHam] = useState(false);
 
-  console.log(ham);
+  console.log(ham, "ham");
   let [navBarMenu, setNavBarMenu] = useState("navbar__menu--hide");
-  let window;
-
-  // FIXME: the event always sets to false need to fix
-  document.addEventListener("click", function (e) {
-    // console.log(e.isTrusted);
-    e.preventDefault();
-    if (ham === true) {
-      return setHam(false);
-    }
-  });
 
   let hamburger = () => {
     if (ham === false) {
@@ -32,6 +23,7 @@ const Navbar = () => {
     <nav className="navbar">
       <div className="navbar__logo-container">
         <a className="navbar__logo-link" href="#/">
+          {/* <p className="navbar__logo">&lt;et&#62;</p> */}
           <img className="navbar__logo" src={Et} alt="ET logo" />
         </a>
         <div onClick={hamburger} className="navbar__mobile">
@@ -40,8 +32,15 @@ const Navbar = () => {
           <div className="navbar__mobile-hamburger navbar__mobile-hamburger-bottom "></div>
         </div>
       </div>
-      <div className={navBarMenu}>
+      <div id="nav__menu" className={navBarMenu}>
         <ul className="navbar__menu-list">
+          <button onClick={() => hamburger()} className="navbar__menu-btn">
+            <img
+              className="navbar__menu-btn-icon"
+              src={Close}
+              alt="Close icon"
+            />
+          </button>
           <li className="navbar__list-item">
             <a className="navbar__list-link" href="#/">
               About Me
