3fb56d2efb6fdc0f46bafeeabd601b72745ba30d
Eliezer Toribio <eliezertor@gmail.com>
2021-02-21 20:45:02 -0500

    parallax version of website

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import "./App.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Navbar from "./Components/Navbar/Navbar";
-import Home from "./Components/Home/Home";
-import Projects from "./Components/Projects/Projects";
-import Contact from "./Components/Contact/Contact";
-import Policy from "./Components/Policy/Policy";
-import Footer from "./Components/Footer/Footer";
-import Error from "./Components/Error/Error";
+import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navbar from './Components/Navbar/Navbar';
+import Home from './Components/Home/Home';
+import Projects from './Components/Projects/Projects';
+import Contact from './Components/Contact/Contact';
+import Policy from './Components/Policy/Policy';
+import Footer from './Components/Footer/Footer';
+import Error from './Components/Error/Error';
+import Intro from './Components/Intro/Intro';
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Navbar />
+        <Intro />
+        {/* <Navbar />
         <Switch>
           <Route exact path="/" component={Home}></Route>
           <Route path="/Projects" component={Projects}></Route>
@@ -20,7 +22,7 @@ function App() {
           <Route exact path="/Policy" component={Policy}></Route>
           <Route component={Error}></Route>
         </Switch>
-        <Footer />
+        <Footer /> */}
       </Router>
     </div>
   );
---
+++ src/Components/Intro/Intro.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import House from '../../assets/icons/house.svg';
+import Door from '../../assets/icons/door2.svg';
+import Tree from '../../assets/icons/tree7.svg';
+import Shrub1 from '../../assets/icons/shrub1.svg';
+import Shrub2 from '../../assets/icons/shrub2.svg';
+import Shrub3 from '../../assets/icons/shrub3.svg';
+import Frame from '../../assets/icons/frame.svg';
+import './intro.scss';
+
+function Intro() {
+  window.addEventListener(
+    'scroll',
+    () => {
+      document.body.style.setProperty(
+        '--scroll',
+        window.pageYOffset / (document.body.offsetHeight - window.innerHeight)
+      );
+    },
+    false
+  );
+
+  return (
+    <div className="intro">
+      <img
+        className="intro__house"
+        src={House}
+        alt="house"
+        // title="intro"
+        // frameborder="0"
+      ></img>
+      <img
+        className="intro__house-door"
+        src={Door}
+        alt="door"
+        // title="door"
+        // frameborder="0"
+      />
+      <img className="intro__door-frame" src={Frame} alt="door frame" />
+      <img className="intro__shrub" src={Shrub3} alt="shrub" />
+      <img className="intro__tree" src={Tree} alt="tree" />
+      <div className="intro__chimney">
+        <div id="box">
+          <div id="chimney-main">
+            <div id="chimney-smoke-box">
+              <div id="chimney-smoke1" className="chimney-smoke"></div>
+              <div id="chimney-smoke2" className="chimney-smoke"></div>
+              <div id="chimney-smoke3" className="chimney-smoke"></div>
+              <div id="chimney-smoke4" className="chimney-smoke"></div>
+              <div id="chimney-smoke5" className="chimney-smoke"></div>
+              <div id="chimney-smoke6" className="chimney-smoke"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Intro;
